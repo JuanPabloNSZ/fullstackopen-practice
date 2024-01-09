999bb0dc1445397585fa7a49556376bdb59bd277
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2024-01-09 10:54:17 -0300

    add two pieces of state (left and right)

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,34 +1,18 @@
 import { useState } from "react";
 
-const Display = ({ counter }) => <div>{counter}</div>;
-
-// Por convención los props de event handlers deben comenzar con 'on' seguido de una letra mayúscula
-// En este caso mantenemos el nombre 'onClick', pero podríamos haberlo nombrado 'onSmash'
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
-
 const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  // Event handlers: Función que se ejecuta cuando ocurre un evento
-  const increaseByOne = () => {
-    setCounter(counter + 1);
-  };
-  const decreaseByOne = () => {
-    setCounter(counter - 1);
-  };
-  const setToZero = () => {
-    setCounter(0);
-  };
+  //* Podemos almacenar más de un estado
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={decreaseByOne} text="minus" />
-      <Button onClick={setToZero} text="reset" />
+      {left}
+      <button onClick={() => setLeft(left + 1)}>left</button>
+      <button onClick={() => setRight(right + 1)}>right</button>
+      {right}
     </div>
   );
-  //* El valor del atributo onClick debe ser una referencia a una función
 };
 
 export default App;
