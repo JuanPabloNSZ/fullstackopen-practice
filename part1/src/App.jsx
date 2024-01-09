e8fc4e3005d7397519f63a66b8742038a6f0e85b
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2024-01-09 20:35:38 -0300

    add conditional rendering

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,22 +1,34 @@
 import { useState } from "react";
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+  return <div>button press history: {props.allClicks.join(" ")}</div>;
+};
+
 const App = () => {
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
   const [allClicks, setAllClicks] = useState([]);
+  const [total, setTotal] = useState(0);
 
   // Event handlers
   //* Es necesario recordar que no se deben mutar directamente los estados
   const handleLeftClick = () => {
     // concat no muta el array sino que crea una copia
     setAllClicks(allClicks.concat("L"));
-    setLeft(left + 1);
+    const updateLeft = left + 1;
+    setLeft(updateLeft);
+    setTotal(updateLeft + right);
   };
 
   const handleRightClick = () => {
     // concat no muta el array sino que crea una copia
     setAllClicks(allClicks.concat("R"));
-    setRight(right + 1);
+    const updateRight = right + 1;
+    setRight(updateRight);
+    setTotal(updateRight + left);
   };
 
   return (
@@ -25,7 +37,8 @@ const App = () => {
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
       {right}
-      <p>{allClicks.join(" ")}</p>
+      <History allClicks={allClicks} />
+      <p>total {total}</p>
     </div>
   );
 };
