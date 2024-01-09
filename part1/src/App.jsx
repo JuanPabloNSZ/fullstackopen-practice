d2f294b9fa7696a74c0d39655a8bb322485aa5e8
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2024-01-09 11:34:17 -0300

    add an object as state

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -2,15 +2,29 @@ import { useState } from "react";
 
 const App = () => {
   //* Podemos almacenar más de un estado
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
+  // const [left, setLeft] = useState(0);
+  // const [right, setRight] = useState(0);
+
+  //* También podemos almacenar un objeto como estado
+  const [clicks, setClicks] = useState({ left: 0, right: 0 });
+
+  // Event handlers
+  const handleLeftClick = () => {
+    const newClicks = { left: clicks.left + 1, right: clicks.right };
+    setClicks(newClicks);
+  };
+
+  const handleRightClick = () => {
+    const newClicks = { left: clicks.left, right: clicks.right + 1 };
+    setClicks(newClicks);
+  };
 
   return (
     <div>
-      {left}
-      <button onClick={() => setLeft(left + 1)}>left</button>
-      <button onClick={() => setRight(right + 1)}>right</button>
-      {right}
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
     </div>
   );
 };
