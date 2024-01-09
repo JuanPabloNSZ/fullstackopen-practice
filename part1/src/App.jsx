123d99200cbb8c17643d322ed4897781353c4d3a
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2024-01-09 03:13:57 -0300

    add event handlers

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -3,13 +3,22 @@ import { useState } from "react";
 const App = () => {
   const [counter, setCounter] = useState(0);
 
-  setTimeout(() => setCounter(counter + 1), 1000);
+  // Event handlers
+  const increaseByOne = () => {
+    setCounter(counter + 1);
+  };
+  const setToZero = () => {
+    setCounter(0);
+  };
 
   return (
     <div>
       <h2>{counter}</h2>
+      <button onClick={increaseByOne}>plus</button>
+      <button onClick={setToZero}>reset</button>
     </div>
   );
+  //* El valor del atributo onClick debe ser una referencia a una función
 };
 
 export default App;
