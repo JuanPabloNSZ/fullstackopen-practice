7d9e382cd02901d1bc2a3dd3c79e01044c2bc392
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2024-01-09 02:52:55 -0300

    add useState

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,45 +1,13 @@
-const Hello = (props) => {
-  //* Para manipular el objeto props
-  //* Podemos almacenar en variables sus valores
-  // const name = props.name;
-  // const age = props.age;
-
-  //* También podemos usar destructuring assignment
-  const { name, age } = props;
-
-  // (La misma función de la branch anterior pero con una sintáxis más compacta)
-  const bornYear = () => new Date().getFullYear() - age;
-
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old.
-      </p>
-      <p>So you were probably born in {bornYear()};</p>
-    </div>
-  );
-};
-
-//* Si llevamos el destructuring un paso más allá, podemos usarlo directamente como parámetro
-const Bye = ({ name, greetings }) => {
-  return (
-    <div>
-      Bye {name}, {greetings}
-    </div>
-  );
-};
+import { useState } from "react";
 
 const App = () => {
-  const name = "Peter";
-  const age = 10;
+  const [counter, setCounter] = useState(0);
+
+  setTimeout(() => setCounter(counter + 1), 1000);
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-      <Bye name="Nikola" greetings="nice to meet you" />
-      <Bye name="Aaron" greetings="have a nice trip" />
+      <h2>{counter}</h2>
     </div>
   );
 };
