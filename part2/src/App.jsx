41b719ae1f9bbcedf671d2fc42322ad8ee8c1129
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2024-01-10 18:21:34 -0300

    add a form to create new notes

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,9 +1,36 @@
+import { useState } from "react";
 import Note from "./components/Note";
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("");
+
+  const addNote = (event) => {
+    // event.preventDefault() evita que la página se actualice al enviar el formulario
+    event.preventDefault();
+
+    const noteObject = {
+      id: notes.length + 1,
+      content: newNote,
+      important: Math.random() < 0.5,
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
   return (
     <div>
       <h1>Notes</h1>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
       <ul>
         {notes.map((x) => (
           <Note key={x.id} content={x.content} />
