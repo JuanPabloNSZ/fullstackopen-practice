249fef165a977a06a76838f84a4a63d87c2bea2e
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2024-01-09 22:43:06 -0300

    add new buttons

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -4,18 +4,36 @@ const App = () => {
   const [value, setValue] = useState(10);
 
   // Esta función devuelve una función
-  const hello = (who) => {
-    const handler = () => console.log("hello", who);
-    return handler;
+  const hello = (who) => () => console.log("hello", who);
+
+  const setToValue = (newValue) => () => {
+    console.log("value now", newValue);
+    setValue(newValue);
   };
 
   return (
     <div>
       {value}
       {/* Podemos llamar a la misma función y personalizarla con distintos argumentos */}
-      <button onClick={hello("world")}>Hello world</button>
-      <button onClick={hello("react")}>Hello react</button>
-      <button onClick={hello("function")}>Hello function</button>
+      <div>
+        <button onClick={hello("world")}>Hello world</button>
+        <button onClick={hello("react")}>Hello react</button>
+        <button onClick={hello("function")}>Hello function</button>
+      </div>
+
+      <div>
+        {/* En este caso los event handlers son: () => setValue(newValue) */}
+        <button onClick={setToValue(0)}>0</button>
+        <button onClick={setToValue(1000)}>1000</button>
+        <button onClick={setToValue(value + 1)}>increment by one</button>
+      </div>
+
+      <div>
+        {/* Sin embargo, podemos lograr la misma funcionalidad de esta manera */}
+        {/* Aunque es una mala práctica definir los event handlers en los atributos de un elemento */}
+        <button onClick={() => setValue(2000)}>2000</button>
+        <button onClick={() => setValue(value + 2)}>increment by two</button>
+      </div>
     </div>
   );
 };
