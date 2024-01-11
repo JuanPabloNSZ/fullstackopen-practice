129007234586eb2f08d7956ed11713f9812ec58e
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2024-01-11 18:00:33 -0300

    add axios, json-server and useEffect to fetch data from db.json

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Note from "./components/Note";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+const App = () => {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/notes").then((x) => {
+      console.log("promise fulfilled");
+      setNotes(x.data);
+    });
+  }, []);
 
   // Event handlers
   const addNote = (event) => {
@@ -12,7 +20,6 @@ const App = (props) => {
     event.preventDefault();
 
     const noteObject = {
-      id: notes.length + 1,
       content: newNote,
       important: Math.random() < 0.5,
     };
